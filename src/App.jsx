fcc0ca2dd71f15d276fd7a19c84e43e1d08b5e19
Bharat Bhandari <bharatbhandari0302@gmail.com>
2023-12-16 23:59:32 +0530

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <h3 className="headLine">SpaceMuleCode</h3>
+      <main>
+        <Hero />
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./css/nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className="navElements">
+        <a href="#gallery">Gallery</a>
+        <a href="#code">Gallery</a>
+        <a href="#contact">Gallery</a>
+        <a href="#about">Gallery</a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
